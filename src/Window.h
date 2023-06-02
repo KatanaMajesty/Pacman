e6f643614cdbecbfc5d4a0275a6912c6f829b4da
katana <sasha.panasenk@gmail.com>
2023-06-02 14:07:07 +0100

    Triangle drawing

--- src/Window.h
+++ src/Window.h
@@ -9,6 +9,7 @@ class Window
 {
 public:
 	Window(uint32_t width, uint32_t height, const std::string& title);
+	~Window();
 
 	bool Open() noexcept;
 	bool Close() noexcept;
@@ -18,6 +19,8 @@ public:
 	constexpr uint32_t GetWidth() const noexcept { return m_width; }
 	constexpr uint32_t GetHeight() const noexcept { return m_height; }
 	const std::string& GetTitle() const noexcept { return m_title; }
+
+	void* GetHandle() noexcept { return m_window.get(); }
 
 private:
 	uint32_t m_width;
