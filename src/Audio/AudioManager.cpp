8480e2bc97a34e3b72c9ca6b3f3bed494de26795
lilarin <80650911+lilarin@users.noreply.github.com>
2023-06-09 00:39:21 +0300

    Added audio manager

--- src/Audio/AudioManager.cpp
+++ src/Audio/AudioManager.cpp
@@ -1,1 +1,45 @@
 #include "AudioManager.h"
+#include "../Utility/FileSystem.h"
+
+AudioManager::AudioManager() 
+{
+    LoadSound(AUDIO_AMBIENCE, "ambience.wav");
+    LoadSound(AUDIO_COIN_COLLECT, "coin_collect.wav");
+    LoadSound(AUDIO_ENEMY_DEATH, "enemy_death.wav");
+    LoadSound(AUDIO_FIGHT, "fight.wav");
+    LoadSound(AUDIO_LEVEL_COMPLATED, "level_complated.wav");
+    LoadSound(AUDIO_LEVEL_FAILED, "level_failed.wav");
+    LoadSound(AUDIO_PLAYER_DEATH, "player_death.wav");
+}
+
+AudioManager& AudioManager::Get()
+{
+    static AudioManager instance;
+    return instance;
+}
+
+void AudioManager::LoadSound(AudioType audioType, const std::string& filename)
+{
+    static std::filesystem::path filepath = (FileSystem::Get().GetAssetsPath() / "Audio");
+    sf::SoundBuffer buffer;
+    if (buffer.loadFromFile((filepath / filename).string())) {
+        m_soundBuffers[audioType] = buffer;
+    }
+}
+
+void AudioManager::PlaySound(AudioType audioType)
+{
+    auto it = m_soundBuffers.find(audioType);
+    if (it != m_soundBuffers.end()) {
+        m_sound.setBuffer(it->second);
+        m_sound.play();
+    }
+}
+
+void AudioManager::StopSound(AudioType audioType) {
+    auto it = m_soundBuffers.find(audioType);
+    if (it != m_soundBuffers.end()) {
+        m_sound.setBuffer(it->second);
+        m_sound.stop();
+    }
+}
