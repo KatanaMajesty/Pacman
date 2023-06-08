2e62c4673930b2ca4f39533ec7f085e74c6e7855
katana <sasha.panasenk@gmail.com>
2023-06-08 15:14:56 +0100

    AABB minor fixes

--- src/Utility/Math.cpp
+++ src/Utility/Math.cpp
@@ -2,8 +2,6 @@
 
 bool BoundingBox::Collide(const BoundingBox& otherBounds) {
 
-    if (m_max.x < otherBounds.m_min.x or m_min.x > otherBounds.m_max.x) { return false; };
-    if (m_max.y < otherBounds.m_min.y or m_min.y > otherBounds.m_max.y) { return false; };
-        
-    return true;
+    return (m_max.x < otherBounds.m_min.x or m_min.x > otherBounds.m_max.x or
+        m_max.y < otherBounds.m_min.y or m_min.y > otherBounds.m_max.y);
 }
